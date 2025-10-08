9f20331059df8fb74c8a188d926bfe434132f497
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-10-08 14:06:33 +0600

    insta image url data  change from json folder ✅

--- src/Pages/Apps.jsx
+++ src/Pages/Apps.jsx
@@ -9,21 +9,25 @@ const Apps = () => {
 
   const [search, setSearch] = useState('')
 
-  const [searchedApps,setSearchedApps] = useState([])
-  
+  const [searchedApps, setSearchedApps] = useState([])
+
   useEffect(() => {
     const term = search.trim().toLowerCase()
-    const searched = term ? apps ?.filter(a => a?.title?.toLowerCase().includes(term)) : apps
+    const searched = term ? apps?.filter(a => a?.title?.toLowerCase().includes(term)) : apps
     setSearchedApps(searched)
-  }, [search,apps])
+  }, [search, apps])
 
 
 
   return (
     <div className='w-10/12 mx-auto p-10 my-10'>
+      <div className='text-center'>
+        <h1 className='text-5xl font-bold mb-5'>Our All Applications</h1>
+        <p className='text-gray-500'>Explore All Apps on the Market developed by us. We code for Millions</p>
+      </div>
 
-      <div className='flex justify-between items-center py-5'>
-        <h1 className='text-2xl font-bold' >({apps.length})Apps Found</h1>
+      <div className='flex justify-between items-center py-8'>
+        <h1 className='text-2xl font-bold' >({apps.length}) Apps Found</h1>
 
         <label className="input">
           <input onChange={(e) => { setSearch(e.target.value) }}
