3abec5ba761f308a332f2a835bffaa98a535b89c
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-10-09 00:02:59 +0600

    update error pages

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link, useRouteError } from 'react-router';
 
+import errorImage from '../assets/404.png'
+
 const ErrorPage = () => {
 
     const error = useRouteError();
 
     return (
         <div className="flex flex-col justify-center items-center h-screen text-center bg-gray-50">
-            <h1 className='xl:lg:md:text-8xl text-lg font-extrabold'>404</h1>
-            <h2 className='text-3xl font-bold text-gray-700 mt-2'>Page Not Found</h2>
+            {/* <h1 className='xl:lg:md:text-8xl text-lg font-extrabold'>404</h1> */}
+            <img className='flex justify-center my-20' src={errorImage} alt="" />
+            <h2 className='text-3xl font-bold text-gray-700 mt-2'>Oops, page not found!</h2>
             <p>
                 {
                     error?.statusText || "The Page you are looking dose not exist"
