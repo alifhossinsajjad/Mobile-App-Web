59735ceaff5f06b3b66329b4a697128c560b710a
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-10-08 14:21:11 +0600

    add a condition apps pages ✅

--- src/Pages/Apps.jsx
+++ src/Pages/Apps.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import useApps from '../Hooks/useApps';
 import AppCard from '../Components/AppCard';
+import { NavLink } from 'react-router';
 
 const Apps = () => {
 
@@ -20,14 +21,14 @@ const Apps = () => {
 
 
   return (
-    <div className='w-10/12 mx-auto p-10 my-10'>
+    <div className='w-11/12 mx-auto p-10 my-10'>
       <div className='text-center'>
         <h1 className='text-5xl font-bold mb-5'>Our All Applications</h1>
         <p className='text-gray-500'>Explore All Apps on the Market developed by us. We code for Millions</p>
       </div>
 
       <div className='flex justify-between items-center py-8'>
-        <h1 className='text-2xl font-bold' >({apps.length}) Apps Found</h1>
+        <h1 className='text-2xl font-bold' >({apps.length || 0}) Apps Found</h1>
 
         <label className="input">
           <input onChange={(e) => { setSearch(e.target.value) }}
@@ -36,11 +37,20 @@ const Apps = () => {
         </label>
 
       </div>
-      <div className='grid xl:lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-20'>
-        {
-          searchedApps.map(app => <AppCard key={app.id} app={app} />)
-        }
-      </div>
+      {searchedApps && searchedApps?.length > 0 ? (
+        <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10">
+          {searchedApps.map(app => (
+            <AppCard key={app.id} app={app} />
+          ))}
+        </div>
+      ) : (
+        <div className="text-center text-gray-500 py-20">
+          <h2 className="text-2xl font-semibold text-gray-700 mb-3">No Apps Found </h2>
+          <div className='flex justify-center '>
+            <NavLink to={'/'} className='bg-gradient-to-tl from-[#9F62F2] to-[#632EE3] flex justify-center items-center text-lg font-bold text-white my-16 p-4 rounded-lg'>Show All</NavLink>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
